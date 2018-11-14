171de09bd85eb15d419d54adb881c820db93cafa
logg926 <logg.com@gmail.com>
2018-11-14 16:50:43 +0800

    Initial Commit

---
+++ Testing1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testing1
+//
+//  Created by Log G Cheng on 14/11/2018.
+//  Copyright © 2018 Log G Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
